a1642d3bcf91265114ee0934b602cd2749b8f3ae
Ali Karani <ali.asghar@by433.com>
2022-10-05 01:00:17 +0500

    Major fixes

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          // className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Native
         </a>
       </header>
     </div>
